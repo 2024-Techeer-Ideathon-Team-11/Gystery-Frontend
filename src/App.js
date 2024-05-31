5756e39aae7c4158bf67bce26966e6339af9cec9
j._.jinoo <jinoo0306@naver.com>
2024-06-01 05:08:20 +0900

    design : 실패 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './page/Main';
 import Game from './page/Game';
+import Fail from './page/Fail';
 import './index.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/game/fail" element={<Fail />} />
       </Routes>
     </Router>
   );
