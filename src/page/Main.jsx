a431f765bb2361a628f5cb0e29d6f36c249d0829
jjini-SUN <shineeseo@naver.com>
2024-06-01 05:12:08 +0900

    design : 초기페이지 퍼블리싱 완료

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import Background from "../components/Background";
 import mainReaper from "../image/mainReaper.png";
 import '../index.css';
+import { useNavigate } from "react-router-dom";
+
 
 function Main(props) {
+  const navigate = useNavigate();
+
   return <div className="w-full h-full flex flex-row items-center justify-center">
           <Background type="main"/>
       <div className = " flex justify-center items-center w-screen h-screen relative overflow-hidden">
         <h3 className="font-custom text-outline text-white text-[142px] absolute top-14 z-10">G Y S T E R Y</h3>
         <div className = "justify-center items-center flex-col flex w-[580px] abssolute">
           <img className=" w-full" src = {mainReaper}></img>
-          <button className = "flex justify-center items-center absolute bottom-14 text-white font-custom text-[96px]  border-4 border-b-white-500 border-t-white-400 border-l-0 border-r-0 pb-2">start</button>
+          <div className = "flex w-[360px] h-[120px] border-2 border-white bg-black bg-opacity-70 rounded-lg rounded-tl-none p-6 text-[20px] absolute right-64 top-80">
+            <p>미스터리 해결의 여정을<br/> 시작하시겠습니까?</p>
+          </div>
+          <button onClick={()=>navigate('/game')}className = "flex justify-center items-center absolute bottom-14 text-white font-custom text-[96px]  border-4 border-b-white-500 border-t-white-400 border-l-0 border-r-0 pb-2">start</button>
         </div>
+        
     </div>
-    <div className = "">
-      <p></p>
-    </div>
+    
   </div>;
 }
 
